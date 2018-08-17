7fe0a41b2e995266222108ff1e4bea6e19c6d7cb
sumanthrao <sraosumanth@gmail.com>
2018-08-17 18:38:51 +0530

    updated reaction

--- reaction.c
+++ reaction.c
@@ -6,23 +6,51 @@
 void make_water();
 
 struct reaction {
-	// FILL ME IN
+	int h;
+	int o;
+	struct lock myLock;
+	struct condition hWait;
+	struct condition oWait;
 };
 
 void
-reaction_init(struct reaction *reaction)
-{
-	// FILL ME IN
+reaction_init(struct reaction *reaction) {
+	reaction -> h = 0;
+	reaction -> o = 0;
+	lock_init(&reaction -> myLock);
+	cond_init(&reaction -> hWait);
+	cond_init(&reaction -> oWait);
 }
 
 void
-reaction_h(struct reaction *reaction)
-{
-	// FILL ME IN
+reaction_h(struct reaction *reaction) {
+	lock_acquire(&reaction -> myLock);
+	(reaction -> h)++;
+	if(reaction -> h >= 2 && reaction -> o >= 1) {
+		(reaction -> h) -= 2;
+		(reaction -> o)--;
+		make_water();
+		cond_signal(&reaction -> hWait, &reaction -> myLock);
+		cond_signal(&reaction -> oWait, &reaction -> myLock);
+	}
+	else
+		cond_wait(&reaction -> hWait, &reaction -> myLock);
+	lock_release(&reaction -> myLock);
 }
 
 void
-reaction_o(struct reaction *reaction)
-{
-	// FILL ME IN
+reaction_o(struct reaction *reaction) {
+	lock_acquire(&reaction -> myLock);
+	(reaction -> o)++;
+	if(reaction -> h >= 2 && reaction -> o >= 1) {
+		(reaction -> h) -= 2;
+		(reaction -> o)--;
+		make_water();
+		cond_signal(&reaction -> hWait, &reaction -> myLock);
+		cond_signal(&reaction -> hWait, &reaction -> myLock);
+		//cond_signal(&reaction -> oWait, &reaction -> myLock);
+	}
+	else
+		cond_wait(&reaction -> oWait, &reaction -> myLock);
+	lock_release(&reaction -> myLock);	
 }
